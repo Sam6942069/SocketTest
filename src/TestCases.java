401b8913e1c3e5a2b59dc273a1ff6f52e04aab77
Sam <sam.kolter@gmx.de>
2022-06-01 16:25:14 +0200

    testConnection updated

--- src/TestCases.java
+++ src/TestCases.java
@@ -7,8 +7,12 @@ import static junit.framework.TestCase.assertEquals;
 
 public class TestCases {
 
+
+
     @Test
     public void testConnection() {
+        Server server = new Server();
+        server.start(6666);
         Client client = new Client();
         client.startConnection("127.0.0.1", 6666);
         String resp = client.sendMessage("hello server");
